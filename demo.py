4e459b8a4d84b67ef12bad44017a395615bba5f2
Shien Wang <63040619+wngsisi@users.noreply.github.com>
2025-03-10 21:26:19 +0800

    Update demo.py
    更改色域范围

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ while True:
     ret, frame = cap.read()
     if not ret:
         break
-
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     cv2.imshow('Camera', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
